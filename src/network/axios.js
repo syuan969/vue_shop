1ee8e2524cdf2d0f93a3a2eea9fb42f80f1a4c9e
sanyuan <2394713718@qq.com>
2022-02-28 15:44:55 +0800

    优化文件结构

--- src/network/axios.js
+++ src/network/axios.js
@@ -1,11 +1,18 @@
 import axios from 'axios'
 import Vue from 'vue'
-
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 axios.defaults.baseURL ="https://lianghj.top:8888/api/private/v1/";
 axios.interceptors.request.use(config=>{
   // console.log(config);
+  NProgress.start()
   config.headers.Authorization=window.sessionStorage.getItem('token')
   // 最后必须return 
+  return config
+})
+// 在reponse拦截器中,隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 Vue.prototype.$http=axios;
